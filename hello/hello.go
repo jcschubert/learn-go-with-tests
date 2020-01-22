58e1e404082b3e51d0576674b60263e02e9e44bd
Jan-Christopher Schubert <jan@jcschubert.com>
2020-01-22 17:57:07 +0100

    Multiple test cases for one function

--- hello/hello.go
+++ hello/hello.go
@@ -6,6 +6,9 @@ const englishPrefix = "Hello, "
 
 // Hello provides a greeting to the world
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return englishPrefix + name
 }
 
