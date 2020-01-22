bd9195f5c5b0aa88c1a8539ebdb068d0ba53668b
Jan-Christopher Schubert <jan@jcschubert.com>
2020-01-22 16:11:16 +0100

    Added tests for hello

--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// Hello provides a greeting to the world
+func Hello() string {
+	return "Hello, world."
+}
+
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(Hello())
 }
