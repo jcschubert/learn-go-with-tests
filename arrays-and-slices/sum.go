a3ada3dbcec8cfbae95086d5fdd8fc03b64469a5
Jan-Christopher Schubert <jan@jcschubert.com>
2020-01-23 19:05:22 +0100

    Iterating over a slice, removed 1 test

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,7 +1,7 @@
 package slices
 
 // Sum returns the sum of all the integers in array numbers.
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
