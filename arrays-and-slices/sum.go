b2626ea6141632a0a03b30fbcdfadf6652137daf
Jan-Christopher Schubert <jan@jcschubert.com>
2020-01-23 19:22:46 +0100

    len() and make(), variadic functions

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -8,3 +8,15 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+// SumAll sums up an array of arrays with numbers, and returns
+// the sums as an array of ints.
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
+}
