94c60dc192a7de0af18236e9ee259e08290366d1
Jan-Christopher Schubert <jan@jcschubert.com>
2020-01-24 10:36:10 +0100

    Structs, Method, Interfaces -- Area

--- struct-methods-interfaces/shapes.go
+++ struct-methods-interfaces/shapes.go
@@ -5,3 +5,9 @@ package shapes
 func Perimeter(width, height float64) float64 {
 	return 2 * (width + height)
 }
+
+// Area calculates the area of the rectangle defined
+// by width and height
+func Area(width, height float64) float64 {
+	return width * height
+}
--- struct-methods-interfaces/shapes_test.go
+++ struct-methods-interfaces/shapes_test.go
@@ -7,6 +7,15 @@ func TestPerimeter(t *testing.T) {
 	want := 40.0
 
 	if got != want {
-		t.Errorf("got %.2f want %2f", got, want)
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	got := Area(10.0, 10.0)
+	want := 100.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
