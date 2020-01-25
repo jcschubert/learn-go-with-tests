cbfaa41010cccbcef6be62b80a0471f4e216394c
Jan-Christopher Schubert <jan@jcschubert.com>
2020-01-25 09:17:03 +0100

    Add error handling to map search

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,11 +1,18 @@
 package maps
 
+import "errors"
+
 // Dictionary represents a dictionary of words, accessible by index.
 type Dictionary map[string]string
 
 // Search returns the string stored under the index 'word' within the dictionary.
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
 }
 
 // Search returns the map[word] from the given map
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -10,8 +10,20 @@ func assertString(t *testing.T, got, want string) {
 }
 
 func TestSearch(t *testing.T) {
-	dict := Dictionary{"test": "this is just a test"}
-	got := dict.Search("test")
-	want := "this is just a test"
-	assertString(t, got, want)
+	t.Run("known word", func(t *testing.T) {
+		dict := Dictionary{"test": "this is just a test"}
+		got, _ := dict.Search("test")
+		want := "this is just a test"
+		assertString(t, got, want)
+	})
+	t.Run("unknown word", func(t *testing.T) {
+		dict := Dictionary{"test": "this is just a test"}
+		_, err := dict.Search("not there")
+		want := "could not find the word you were looking for"
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertString(t, err.Error(), want)
+	})
 }
