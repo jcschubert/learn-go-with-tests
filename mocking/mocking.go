eb837a4f3b6a7ac30b6ee492796141389ccc1796
Jan-Christopher Schubert <jan@jcschubert.com>
2020-01-26 21:48:59 +0100

    Added sleep for countdown

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -4,17 +4,20 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-const finalWord := "Go!"
-const countDownStart := 3
+const finalWord = "Go!"
+const countDownStart = 3
 
 // Countdown counts down and prints messages, waiting
 // 1 second between each message
 func Countdown(out io.Writer) {
 	for i := countDownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 	}
+	time.Sleep(1 * time.Second)
 	fmt.Fprintf(out, finalWord)
 }
 
