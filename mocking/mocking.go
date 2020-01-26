a87d4cf1d625f30912ec0f53ea9e6f4e9b095220
Jan-Christopher Schubert <jan@jcschubert.com>
2020-01-26 21:59:15 +0100

    Sleeper mock

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -10,17 +10,42 @@ import (
 const finalWord = "Go!"
 const countDownStart = 3
 
+// Sleeper allows us to construct a mock for time.Sleep
+type Sleeper interface {
+	Sleep()
+}
+
+// DefaultSleeper wraps time.Sleep in an attached method
+type DefaultSleeper struct {
+}
+
+// Sleep just calls sleep
+func (s *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+// SpySleeper tracks the number of calls to it.
+type SpySleeper struct {
+	NumCalls int
+}
+
+//
+func (s *SpySleeper) Sleep() {
+	s.NumCalls++
+}
+
 // Countdown counts down and prints messages, waiting
 // 1 second between each message
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countDownStart; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprintf(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	s := &DefaultSleeper{}
+	Countdown(os.Stdout, s)
 }
