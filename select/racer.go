4e3f93dce59ddb2b964205668fb2cc4c0c7e03fb
Jan-Christopher Schubert <jan@jcschubert.com>
2020-01-27 19:56:39 +0100

    Use select and channels

--- select/racer.go
+++ select/racer.go
@@ -1,21 +1,22 @@
 package racer
 
-import (
-	"net/http"
-	"time"
-)
+import "net/http"
 
 // Racer takes two urls a and b and returns the one with faster response time.
-func Racer(a, b string) string {
-	if responseTime(a) < responseTime(b) {
+func Racer(a, b string) (winner string) {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-
-	return b
 }
 
-func responseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
