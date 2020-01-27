46b9fab4fde4dbc6f71f37398c53ab2c4a36e2fa
Jan-Christopher Schubert <jan@jcschubert.com>
2020-01-27 19:04:48 +0100

    TestRacer with httptest.newServer

---
+++ select/racer_test.go
@@ -0,0 +1,25 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	got := Racer(slowServer.URL, fastServer.URL)
+
+	if got != fastServer.URL {
+		t.Errorf("got %q, want %q", got, fastServer.URL)
+	}
+}
