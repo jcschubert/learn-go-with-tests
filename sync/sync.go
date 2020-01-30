9fa7a2532ca3d0d246a06756ecc62ff71ad6581b
Jan-Christopher Schubert <jan@jcschubert.com>
2020-01-30 17:45:44 +0100

    Refactored the tests

--- sync/sync.go
+++ sync/sync.go
@@ -1,13 +1,16 @@
 package sync
 
+// Counter represents a that can be incremented and read out.
 type Counter struct {
 	value int
 }
 
+// Inc increments the counter by 1
 func (c *Counter) Inc() {
 	c.value++
 }
 
+// Value returns the counter's current value
 func (c *Counter) Value() int {
 	return c.value
 }
