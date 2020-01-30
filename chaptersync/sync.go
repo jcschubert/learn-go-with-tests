ca5cc90e555fcf01f888379b33a40c9af261c610
Jan-Christopher Schubert <jan@jcschubert.com>
2020-01-30 18:12:49 +0100

    Concurrent test case + mutex

--- chaptersync/sync.go
+++ chaptersync/sync.go
@@ -1,12 +1,17 @@
-package sync
+package chaptersync
+
+import "sync"
 
 // Counter represents a that can be incremented and read out.
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
 // Inc increments the counter by 1
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
