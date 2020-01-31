ee9d0c007c2988f8ae35cc51cb7c714c14f1182c
Jan-Christopher Schubert <jan@jcschubert.com>
2020-01-31 17:03:18 +0100

    hoursInRadians + Test 3

--- maths/clockface.go
+++ maths/clockface.go
@@ -85,5 +85,5 @@ func minutesInRadians(t time.Time) float64 {
 }
 
 func hoursInRadians(t time.Time) float64 {
-	return (math.Pi / (6 / float64(t.Hour()))) 
+	return (math.Pi / (6 / float64(t.Hour() % 12))) 
 }
