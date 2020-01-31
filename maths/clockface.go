0f636bc06d6c73492e01e53ad6ee8f8b65246b89
Jan-Christopher Schubert <jan@jcschubert.com>
2020-01-31 15:32:34 +0100

    SVGWriter + Acceptance test

--- maths/clockface.go
+++ maths/clockface.go
@@ -1,6 +1,8 @@
 package clockface
 
 import (
+	"io"
+	"fmt"
 	"math"
 	"time"
 )
@@ -18,14 +20,33 @@ const (
 	clockCentreY     = 150
 )
 
-// SecondHand is the unit vector of the second hand of an analogue clock
-// represented as a Point
-func SecondHand(t time.Time) Point {
+// SVG snippets
+const (
+	svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+	<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+	<svg xmlns="http://www.w3.org/2000/svg"
+    	width="100%"
+    	height="100%"
+    	viewBox="0 0 300 300"
+		version="2.0">`
+	secondHand = `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`
+	bezel = `<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
+	svgEnd = `</svg>`
+)
+
+func SVGWriter(w io.Writer, t time.Time) {
+	io.WriteString(w, svgStart)
+	io.WriteString(w, bezel)
+	SecondHand(w, t)
+	io.WriteString(w, svgEnd)
+}
+
+func SecondHand(w io.Writer, t time.Time) {
 	p := secondHandPoint(t)
 	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // translate
 	p = Point{p.X, -p.Y}                                      // flip
 	p = Point{p.X + clockCentreX, p.Y + clockCentreY}         // translate
-	return p
+	fmt.Fprintf(w, secondHand, p.X, p.Y)
 }
 
 func secondHandPoint(t time.Time) Point {
