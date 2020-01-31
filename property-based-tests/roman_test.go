64047132c39c0435eb14374c3158f5877553fc20
Jan-Christopher Schubert <jan@jcschubert.com>
2020-01-31 10:36:32 +0100

    Convert 5

--- property-based-tests/roman_test.go
+++ property-based-tests/roman_test.go
@@ -14,18 +14,10 @@ func TestRomanNumerals(t *testing.T) {
 		{"2 gets converted to II", 2, "II"},
 		{"3 gets converted to III", 3, "III"},
 		{"4 gets converted to IV", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
 	}
 
 	testCases(t, cases, ConvertToRoman)
-	/*
-		for _, test := range cases {
-			t.Run(test.Description, func(t *testing.T) {
-				got := ConvertToRoman(test.Arabic)
-				if got != test.Want {
-					t.Errorf("got %q, want %q", got, test.Want)
-				}
-			})
-		}*/
 }
 
 func testCases(t *testing.T, cases []RomanLiteralCases, f func(int) string) {
