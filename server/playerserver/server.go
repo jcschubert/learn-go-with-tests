7486e21a78eb99e3e7f8e7dda9c3d5f47673a7ee
Jan-Christopher Schubert <jan@jcschubert.com>
2020-02-01 19:53:35 +0100

    Added second player

--- server/playerserver/server.go
+++ server/playerserver/server.go
@@ -3,10 +3,21 @@ package playerserver
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // PlayerServer returns the amount of the player's wins as a string
 // Example return values: "20", "0", "123"
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	if player == "Pepper" {
+		fmt.Fprintf(w, "20")
+		return
+	}
+
+	if player == "Floyd" {
+		fmt.Fprint(w, "10")
+		return
+	}
 }
