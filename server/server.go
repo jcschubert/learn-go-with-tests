5c152bec3f977a889883e1164fd9030c6cf05e4f
Jan-Christopher Schubert <jan@jcschubert.com>
2020-02-01 17:12:04 +0100

    PlayerServer initial commit

---
+++ server/server.go
@@ -0,0 +1,12 @@
+package buildingawebapp
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// PlayerServer returns the amount of the player's wins as a string
+// Example return values: "20", "0", "123"
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "20")
+}
