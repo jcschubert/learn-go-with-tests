750b8b775cf8136821f02fff2e42c79c6fb11f8f
Jan-Christopher Schubert <jan@jcschubert.com>
2020-02-01 20:03:12 +0100

    Refactored tests

--- server/playerserver/server.go
+++ server/playerserver/server.go
@@ -10,14 +10,19 @@ import (
 // Example return values: "20", "0", "123"
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Fprintf(w, GetPlayerScore(player))
+}
 
-	if player == "Pepper" {
-		fmt.Fprintf(w, "20")
-		return
+// GetPlayerScore returns the score for the provided player name.
+// If there is no such player, it returns an empty string.
+func GetPlayerScore(name string) string {
+	if name == "Pepper" {
+		return "20"
 	}
 
-	if player == "Floyd" {
-		fmt.Fprint(w, "10")
-		return
+	if name == "Floyd" {
+		return "10"
 	}
+
+	return ""
 }
