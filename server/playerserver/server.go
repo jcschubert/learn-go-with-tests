7d642a444d105066fb859b90900f6e5666faecfa
Jan-Christopher Schubert <jan@jcschubert.com>
2020-02-13 13:46:03 +0100

    Refactoring. Extract player from url

--- server/playerserver/server.go
+++ server/playerserver/server.go
@@ -19,16 +19,17 @@ type PlayerServer struct {
 // Example return values: "20", "0", "123"
 // returns status 404 if player not found
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
 	switch r.Method {
 	case http.MethodPost:
-		p.processWin(w, r)
+		p.processWin(w, player)
 	case http.MethodGet:
-		p.postScore(w, r)
+		p.showScore(w, player)
 	}
 }
 
-func (p *PlayerServer) postScore(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 	score := p.Store.GetPlayerScore(player)
 
 	if score == 0 {
@@ -38,8 +39,7 @@ func (p *PlayerServer) postScore(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, score)
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
 	p.Store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
 }
