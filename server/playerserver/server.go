db334169e679e3912c9ec86e27901b7c935f5605
Jan-Christopher Schubert <jan@jcschubert.com>
2020-02-13 21:04:38 +0100

    Added storage integration test

--- server/playerserver/server.go
+++ server/playerserver/server.go
@@ -6,6 +6,14 @@ import (
 	"strings"
 )
 
+type InMemoryPlayerStore struct{}
+
+func (s *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
+func (s *InMemoryPlayerStore) RecordWin(name string) {}
+
 type PlayerStore interface {
 	GetPlayerScore(name string) int
 	RecordWin(name string)
